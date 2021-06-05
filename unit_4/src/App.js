b1a8365518f1e029cb4fb24a15b497670fc45c80
Dmitriy <dmitriyvalerievich24@mail.ru>
2021-06-05 13:24:31 +0300

    unit 4 test

---
+++ unit_4/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Comp1 from "./components/Test1";
+
+function App() {
+  return (
+    <>
+      <Comp1 arg={77} />
+    </>
+  );
+}
+
+export default App;
---
+++ unit_4/src/components/Test1.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Test1 extends React.Component {
+  constructor(props) {
+    console.clear();
+    console.log("constructor");
+    console.log(props);
+    super();
+    this.state = {
+      s1: 0,
+    };
+  }
+  // dd
+  ButtonHandler = () => {
+    let val = this.state.s1;
+    val++;
+    this.setState({ s1: val });
+  };
+
+  static getDrivedStateFromProps(props, state) {
+    console.log("get drived state");
+    return null;
+    // return ({'s1' : props.arg})
+  }
+
+  render() {
+    console.log("render 1");
+
+    return (
+      <>
+        {console.log("render 2")}
+        <div>
+          <button onClick={this.ButtonHandler}>Push button</button>
+        </div>
+        <div>{this.state.s1}</div>
+      </>
+    );
+  }
+}
+
+export default Test1;
